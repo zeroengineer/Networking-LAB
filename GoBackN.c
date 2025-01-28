1630e74ae52af011a42922dd18fade76ce020f8c
Devang <iamdevangcv@gmail.com>
2025-01-28 09:58:56 +0530

    Update GoBackN.c

--- GoBackN.c
+++ GoBackN.c
@@ -1,48 +1,56 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
+
 int main()
 {
-  int nf,N,x;
-  int tr=0;
+  int nf, N, x;
+  int tr = 0;
   srand(time(NULL));
+  
   printf("Enter the number of frames: ");
-  scanf("%d",&nf);
+  scanf("%d", &nf);
+  
   printf("Enter the Window Size: ");
   scanf("%d", &N);
-  int i=1;
-  while(i<=nf)
+  
+  int i = 1;
+  
+  while(i <= nf)
   {
-    int x=0;
-    for(int j=i;j<i+N && j<=n;j++)
+    x = 0; // Reset x for each window of frames
+    
+    // Send frames in the window
+    for(int j = i; j < i + N && j <= nf; j++)
     {
       printf("Sent Frame %d \n", j);
       tr++;
     }
-//After each frame is send, increment tr by 1
-//to track total number of transmissions
+
+    // After sending frames, check for acknowledgment
+    for(int j = i; j < i + N && j <= nf; j++)
+    {
+      int flag = rand() % 2;  // Simulate random frame loss (0 = received, 1 = lost)
+      
+      if (!flag)  // Frame successfully received
+      {
+        printf("%d: Acknowledged! \n", j);
+        x++;  // Increment x when a frame is successfully acknowledged
+      }
+      else  // Frame lost, need retransmission
+      {
+        printf("Frame %d Not Received \n", j);
+        printf("Retransmitting Window \n");
+        break;  // Break out of the loop to retransmit the window
+      }
+    }
+    
+    i += x;  // Update i by the number of successfully acknowledged frames
+    printf("\n");
   }
-  for(int j=i;j<i+N && j<=nf;j++)
-  {
-    int flag=rand()%2;//lost frame set as frame 2
-    if(!flag)
-    {
-      (printf("%d: Acknowledged! \n", j);
-      x++;
-    }
-//After acknowledging frame, increment x indicating
-//success of frame transmission
-  else
-    {
-      printf("Frame %d Not Received \n", i);
-      printf("Retransmitting Window \n");
-      break;
-    }
-  printf("\n");
-  i+=x;
-  //i updates number of successful
-  //transfers all acknowledged frames
-}
+  
   printf("Total number of transmissions: %d \n", tr);
+  
   return 0;
 }
+
